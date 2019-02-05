af982ed26b188d286bf1e2050580db7c7cada45c
DivyaJyothiKasam <divya.kasam@wipro.com>
2019-02-05 05:36:52 +0000

    Added new Kernel 3.10.0-957.5.1.el7.x86_64 for RHEL76

--- LISISO/CentOS76/upgrade.sh
+++ LISISO/CentOS76/upgrade.sh
@@ -5,11 +5,14 @@
 ################################################################
 kernelver=`uname -r`
 regex1='3.10.0-957.1.3.el7.x86_64'
+regex2='3.10.0-957.5.1.el7.x86_64'
 
 source ../commonfunctions.sh
 
 if [[ "$kernelver" =~ $regex1 ]]; then	        
-	upgradebuildrpm 1   
+	upgradebuildrpm 1
+if [[ "$kernelver" =~ $regex2]]; then
+        upgradebuildrpm 2
 elif [ "$kernelver" == "3.10.0-957.el7.x86_64" ] ;then
 	checkrpms
 	upgradebuildrpm   
