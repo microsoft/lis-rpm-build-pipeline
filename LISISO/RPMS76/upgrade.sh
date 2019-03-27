e53a072eb707d547fcda41c3b61cea218e1fd6df
DivyaJyothiKasam <divya.kasam@wipro.com>
2019-03-27 06:08:34 +0000

    Add Kernel 3.10.0-957.10.1.el7.x86_64 for RH76

--- LISISO/RPMS76/upgrade.sh
+++ LISISO/RPMS76/upgrade.sh
@@ -6,6 +6,7 @@
 kernelver=`uname -r`
 regex1='3.10.0-957.1.3.el7.x86_64'
 regex2='3.10.0-957.5.1.el7.x86_64'
+regex3='3.10.0-957.10.1.el7.x86_64'
 
 source ../commonfunctions.sh
 
@@ -13,6 +14,8 @@ if [[ "$kernelver" =~ $regex1 ]]; then
 	upgradebuildrpm 1
 elif [[ "$kernelver" =~ $regex2 ]]; then
         upgradebuildrpm 2
+elif [[ "$kernelver" =~ $regex3 ]]; then
+        upgradebuildrpm 3
 elif [ "$kernelver" == "3.10.0-957.el7.x86_64" ] ;then
 	checkrpms
 	upgradebuildrpm   
