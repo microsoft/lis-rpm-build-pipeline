microsoft/lis-rpm-build-pipeline

commit d67b88d67b4b4c7efe4b428fb99e0ee5ec10e82e

RH7: Dynamic detection of LIS compatibility with installed kernel
This commit will leverage the dynamic detection of LIS compatibility
with installed kernel during LIS installation. When the new errata
kernel is compatible then LIS package will be installed.

LISISO/upgrade.sh
@@ -44,6 +44,8 @@ distro_version="unknown"
 
 
 source commonfunctions.sh
+export LIS_INSTALL_BASE_DIR=$(pwd)
+
 #
 # Main script body
 #
@@ -86,28 +88,8 @@ if [[ $distro_version != "5"* ]]; then
 	[ $(rpm -q kernel | wc -l) -eq 1 ] && export no_initramfs=1
 fi
 
-if [[ $distro_version == "7"* ]]; then
- if [ -e /etc/modprobe.d/hyperv_pvdrivers.conf ]; then
-  mv /etc/modprobe.d/hyperv_pvdrivers.conf /opt/files/
- fi
-fi
-
 cd ${targetDir}
-
-#
-# If the daemons are left installed, the new rpms will fail to install
-#
-RemoveHypervDaemons
-
-#
-# Hyper-V tools conflicts with installation, removing the tool
-#
-RemoveHypervTools
-
-
-#
 # Invoke the release specific install script
-#
 echo "Invoking release specific install file in directory ${targetDir}"
 ./upgrade.sh
 

LISISO/commonfunctions.sh
@@ -171,7 +171,7 @@ IsKernelSupported()
 {
 	Distro=$1
 	InstalledKernVersion=$(uname -r)
-	SupportedVersion=`cat latestsupportedkernel | grep $Distro | cut -d: -f2`
+	SupportedVersion=`cat ${LIS_INSTALL_BASE_DIR}/latestsupportedkernel | grep $Distro | cut -d: -f2`
 	[ x"$SupportedVersion" == x ] && return 1
 
 	SupportedVersion=${SupportedVersion//\.el*.*/}
@@ -281,15 +281,51 @@ RemoveHypervTools()
 	fi
 }
 
+function RemoveConflictingModules()
+{
+	if [[ $distro_version == "7"* ]]; then
+		if [ -e /etc/modprobe.d/hyperv_pvdrivers.conf ];then
+			mv /etc/modprobe.d/hyperv_pvdrivers.conf /opt/files/
+		fi
+	fi
+	# If the daemons are left installed, the new rpms will fail to install
+	RemoveHypervDaemons
+
+	# Hyper-V tools conflicts with installation, removing the tool
+	RemoveHypervTools
+}
+
+function CheckLISCompatibility()
+{
+	RunLISCompatibilityCheck
+	ret=$?
+	if [ $ret -eq 2 ];then
+		# Fallback mechanism. Using static check.
+		IsKernelSupported "RHEL$distro_version"
+		[ $? -eq 0 ] &&
+			echo "Kernel-$(uname -r) not supported, Exiting." && exit 1
+	elif [ $ret -eq 0 ];then
+		echo -e "\nKernel-$(uname -r) not supported, installation aborted.\n"
+		exit 1
+	fi
+}
 
 function installbuildrpm()
 {
 	i=$1
+	source ${LIS_INSTALL_BASE_DIR}/lis_compatibility_check.sh
+	GetDistroVersion
 	cd "update${i}" &> /dev/null
 	kmodrpm=`ls kmod-microsoft-hyper-v-*.x86_64.rpm`
 	msrpm=`ls microsoft-hyper-v-*.x86_64.rpm`
 	if [ "$kmodrpm" != "" ] && [ "$msrpm" != ""  ];
 	then
+		# Check lis compatibility with kernel
+		[[ $distro_version == "7"* ]] && CheckLISCompatibility
+
+		# Remove conflicting module before installation
+		RemoveConflictingModules
+
 		echo "Installing the Linux Integration Services for Microsoft Hyper-V..."
 		rpm -ivh $kmodrpm $msrpm 2>&1 | grep -v "mlx5_ib"
 		kmodexit=$?
@@ -306,11 +342,19 @@ function installbuildrpm()
 function upgradebuildrpm()
 {
 	i=$1
+	source ${LIS_INSTALL_BASE_DIR}/lis_compatibility_check.sh
+	GetDistroVersion
 	cd "update${i}" &> /dev/null
 	kmodrpm=`ls kmod-microsoft-hyper-v-*.x86_64.rpm`
 	msrpm=`ls microsoft-hyper-v-*.x86_64.rpm`
 	if [ "$kmodrpm" != "" ] && [ "$msrpm" != ""  ];
 	then
+		# Check lis compatibility with kernel
+		[[ $distro_version == "7"* ]] && CheckLISCompatibility
+
+		# Remove conflicting module before installation
+		RemoveConflictingModules
+
 		rpm -Uvh $kmodrpm $msrpm 2>&1 | grep -v "mlx5_ib"
 	        msexit=$?
         	if [ "$msexit" != 0 ]; then

LISISO/install.sh
@@ -51,6 +51,7 @@ distro_name="unknown"
 distro_version="unknown"
 
 source commonfunctions.sh
+export LIS_INSTALL_BASE_DIR=$(pwd)
 
 #
 # Main script body
@@ -78,12 +79,6 @@ if [ $distro_name = "Oracle" ]; then
 	fi
 fi
 
-IsKernelSupported "RHEL$distro_version"
-if [ $? -eq 0 ];then
-	echo "Kernel version not supported, Exiting"
-	exit 1
-fi
-
 if [[ $distro_version != "5"* ]]; then
  [ $(rpm -q kernel | wc -l) -eq 1 ] && export no_initramfs=1
  latestkernel=(`rpm -q kernel | tail -n1 | cut -c 8-`)
@@ -101,32 +96,13 @@ if [ ! -e "./${targetDir}" ]; then
 	exit 1
 fi
 
-if [[ $distro_version == "7"* ]]; then
- if [ -e /etc/modprobe.d/hyperv_pvdrivers.conf ]; then
-  mv /etc/modprobe.d/hyperv_pvdrivers.conf /opt/files/
- fi
-fi
-
 cd ${targetDir}
-
 if [[ $distro_version != "5"* ]]; then
 	CheckRequiredSpace
 	[ $? -eq 0 ] && exit 1
 fi
 
-#
-# If the daemons are left installed, the new rpms will fail to install
-#
-RemoveHypervDaemons
-
-#
-# Hyper-V tools conflicts with installation, removing the tool
-#
-RemoveHypervTools
-
-#
 # Invoke the release specific install script
-#
 echo "Invoking release specific install file in directory ${targetDir}"
 ./install.sh
 

LISISO/latestsupportedkernel
@@ -2,5 +2,5 @@ RHEL5:2.6.18-419.x86_64
 RHEL6:2.6.32-754.14.2.el6.x86_64
 RHEL73:3.10.0-514.64.2.el6.x86_64
 RHEL74:3.10.0-693.47.2.el7.x86_64
-RHEL75:3.10.0-862.32.2.el7.x86_64
-RHEL76:3.10.0-957.12.2.el7.x86_64
+RHEL75:3.10.0-862.34.1.el7.x86_64
+RHEL76:3.10.0-957.21.2.el7.x86_64

LISISO/lis_compatibility_check.sh
@@ -1,0 +1,76 @@
+#!/bin/bash
+
+# Script to detect the lis compatibility with running kernel.
+# Script returns the following status
+#  	0 : Not compatible
+#  	1 : Compatible
+#  	2 : Cannot perform compatibility check
+
+depmod_conf="/etc/depmod.d/dist.conf"
+kernelver=$(uname -r)
+verbose=1
+
+tmp_dir=$(mktemp -td .lis_compat_chk.XXXXXX)
+trap "rm -rf $tmp_dir" EXIT
+
+function PrepareSandboxForCompatCheck()
+{
+	[[ ! -d $tmp_dir ]] &&
+		echo "Failed to create tmp dir. Skipping compatibility check." && exit 2
+
+	[[ ! -f "/boot/symvers-${kernelver}.gz" ]] &&
+		echo "symvers-${kernelver} not found" && return 2
+
+	zcat /boot/symvers-${kernelver}.gz > $tmp_dir/symvers
+
+	mkdir -p $tmp_dir/etc/depmod.d/
+	[[ -f $depmod_conf ]] && cp $depmod_conf $tmp_dir/etc/depmod.d/ ||
+		echo "search updates extra built-in weak-updates" > $tmp_dir$depmod_conf
+}
+
+function ConfigureModulesForCompatCheck()
+{
+	working_dir=$(pwd)
+	lis_kmod_rpm=$(ls kmod-microsoft-hyper-v-*.x86_64.rpm 2> /dev/null)
+
+	# Install lis rpm into a folder for dependency check
+	rpm -i --root $tmp_dir --nodeps --notriggers --noscripts $lis_kmod_rpm
+	[ $? -ne 0 ] && echo "Failed to install $lis_kmod_rpm" && return 2
+
+	# Create a copy of lib module folder for installed kernel.
+	kmod_kernel_ver=$(ls $tmp_dir/lib/modules/ 2> /dev/null)
+	cd $tmp_dir/lib/modules
+	ln -s $kmod_kernel_ver $kernelver
+
+	# Create a symbolic link to drivers of installed kernel.
+	cd $kernelver
+	ln -sf /lib/modules/$kernelver/kernel kernel
+
+	cd $working_dir
+}
+
+function RunLISCompatibilityCheck()
+{
+	PrepareSandboxForCompatCheck
+	ret=$?
+	[ $ret -ne 0 ] && return $ret
+
+	ConfigureModulesForCompatCheck
+	ret=$?
+	[ $ret -ne 0 ] && return $ret
+
+	depmod_output_log=$(mktemp -p $tmp_dir)
+	depmod -b $tmp_dir -ae -E $tmp_dir/symvers -C $tmp_dir/etc/depmod.d/ $kernelver 1> $depmod_output_log 2>&1
+
+	# Check for the warning reported by the microsoft-hyper-v drivers
+	if [ $verbose -eq 1 ];then
+		cat $depmod_output_log | grep "microsoft-hyper-v" |
+			 grep "disagrees about version of symbol" 2>&1
+	else
+		cat $depmod_output_log | grep "microsoft-hyper-v" |
+			 grep "disagrees about version of symbol" 1> /dev/null 2>&1
+	fi
+	ret=$?
+
+	return $ret
+}

commit 09a4d90a8485015042559954e787421cb6573c20

RH6: Removed unwanted modules during LIS upgrade
This modification is required to remove the unwanted modules when LIS is
upgraded without installation.

Fixes: d67b88d67 ("RH7: Dynamic detection of LIS compatibility with
installed kernel")

LISISO/upgrade.sh
@@ -89,6 +89,12 @@ if [[ $distro_version != "5"* ]]; then
 fi
 
 cd ${targetDir}
+
+if [[ $distro_version != "7"* ]]; then
+	# Remove conflicting module before installation
+	RemoveConflictingModules
+fi
+
 # Invoke the release specific install script
 echo "Invoking release specific install file in directory ${targetDir}"
 ./upgrade.sh